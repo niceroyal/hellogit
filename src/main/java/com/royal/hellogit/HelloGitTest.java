050799d2717a184ce7377f1b79acaf0f7a2b67ad
royal <272506759@qq.com>
2019-11-23 17:32:22 +0800

    test

---
+++ src/main/java/com/royal/hellogit/HelloGitTest.java
@@ -0,0 +1,16 @@
+package com.royal.hellogit;
+
+/**
+ * @ClassName HelloGitTest
+ * @Description TODO
+ * @Author royal
+ * @Date 2019/11/23 17:26
+ * @Version 1.0
+ */
+public class HelloGitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git test!");
+    }
+
+}
